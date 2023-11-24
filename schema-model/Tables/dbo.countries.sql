082b78d2a4bb0ebaa9cd99e720a029c1549323b5
Ľuboš Michalič <lubos.michalic@red-gate.com>
2023-11-24 11:00:21 +0000

    Update dbo.countries.sql

--- schema-model/Tables/dbo.countries.sql
+++ schema-model/Tables/dbo.countries.sql
@@ -2,7 +2,8 @@ CREATE TABLE [dbo].[countries]
 (
 [country_code] [char] (2) NOT NULL,
 [country_name] [varchar] (25) NOT NULL,
-[foo] [char] (2) NULL
+[foo] [char] (2) NULL,
+[bar] [char] (2) NULL
 )
 GO
 ALTER TABLE [dbo].[countries] ADD CONSTRAINT [PK__countrie__3436E9A466260FBE] PRIMARY KEY CLUSTERED ([country_code])
