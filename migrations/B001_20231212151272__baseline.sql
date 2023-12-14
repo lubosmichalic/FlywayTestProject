18740f384dd4416d0f02db467bec0bc2e79314cd
Lubos.Michalic <Lubos.Michalic@red-gate.com>
2023-12-14 11:13:17 +0000

    dfs

---
+++ migrations/B001_20231212151272__baseline.sql
@@ -0,0 +1,19 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Creating [dbo].[Table_1]'
+GO
+CREATE TABLE [dbo].[Table_1]
+(
+[foo] [nchar] (10) NULL
+)
+GO
+PRINT N'Creating [dbo].[Table_2]'
+GO
+CREATE TABLE [dbo].[Table_2]
+(
+[bar] [nchar] (10) NULL
+)
+GO
+
---
+++ schema-model/Tables/dbo.Table_1.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[Table_1]
+(
+[bar
+bar] [nchar] (10) NULL,
+[foo] [nchar] (10) NULL
+)
+GO
